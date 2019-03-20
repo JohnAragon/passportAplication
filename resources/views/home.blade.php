666acd5981c2de2b5cb87ac19ae07ad372f1493d
vagrant <vagrant@homestead>
2019-03-20 21:17:09 +0000

    Componentes vue.js passport

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,21 +3,19 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
+       
+            <div class="container">
+                <passport-clients></passport-clients>
             </div>
-        </div>
+
+             <div class="container">
+                <passport-authorized-clients></passport-authorized-clients>
+            </div>
+
+             <div class="container">
+                <passport-personal-access-tokens></passport-personal-access-tokens>
+            </div>
+       
     </div>
 </div>
 @endsection
